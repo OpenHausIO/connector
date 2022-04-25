976e02b3ca2075604e047a4f20b906a1053be377
Marc Stirner <marc.stirner@it2media.de>
2022-04-25 16:32:56 +0200

    stream added

--- sockets/tcp.js
+++ sockets/tcp.js
@@ -1,15 +1,11 @@
-// this file handles tcp "sockets"
-// forward only payload between backend & socket
-
 const net = require("net");
 
-module.exports = (host, port) => {
+module.exports = ({ host, port }) => {
 
     let socket = new net.Socket();
 
     socket.on("error", (err) => {
-        //cb(err);
-        console.error(`[error] tcp://${host}:${port}`, err)
+        console.error(`[error] tcp://${host}:${port}`, err);
     });
 
     socket.on("close", () => {
@@ -17,7 +13,7 @@ module.exports = (host, port) => {
     });
 
     socket.on("connect", () => {
-        console.error(`[connected] tcp://${host}:${port}`)
+        console.error(`[connected] tcp://${host}:${port}`);
     });
 
     socket.connect(port, host);
