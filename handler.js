0e509f0147cfe61828d18305e84b2c9d5b3162a8
mStirner <marc.stirner@hotmail.de>
2022-08-23 21:32:12 +0200

    bridging process run in worker thread

--- handler.js
+++ handler.js
@@ -1,82 +1,73 @@
 const url = require("url");
-const wsStream = require("./ws-stream.js");
+const {
+    Worker
+} = require("worker_threads");
 
-/**
- * @function bridge
- * Birdges a WebSocket endpoint to a network socket
- * 
- * @param {String} uri WebSocket URL endpoint
- */
-function bridge(uri, settings) {
-    try {
+const MAPPINGS = new Map();
 
-        let retry = false;
 
-        let upstream = wsStream(uri);
-        let stream = require(`./sockets/${settings.socket}`)(settings);
+async function spawn(url2, settings) {
 
-        //@TODO implement "waiting for data from upstream"
-        // if socket stream closes (successful)
-        // re-create one or wait for upstream data?
-
-        upstream.on("error", (err) => {
-            console.error("Could not bridge interface", settings, err);
-        });
-
-        upstream.on("close", () => {
-            if (!retry) {
-
-                retry = true;
-
-                setTimeout(() => {
-
-                    stream.end();
-                    bridge(uri, settings);
-
-                }, Number(process.env.RECONNECT_DELAY));
-
-            }
-        });
-
-        stream.on("close", () => {
-            if (!retry) {
-
-                retry = true;
-
-                setTimeout(() => {
-
-                    upstream.end();
-                    bridge(uri, settings);
-
-                }, Number(process.env.RECONNECT_DELAY));
-
-            }
-        });
-
-        upstream.pipe(stream);
-        stream.pipe(upstream);
-
-    } catch (err) {
-
-        console.error("Error happend %s", uri, err);
-
+    if (MAPPINGS.has(url2)) {
+        console.log("Terminating exisitng bridge!", url2);
+        await MAPPINGS.get(url2).terminate();
     }
+
+    let worker = new Worker("./bridge.js", {
+        workerData: {
+            upstream: String(url2),
+            settings,
+            options: {}
+        }
+    });
+
+    worker.on("error", (err) => {
+        console.log("Worker error", err);
+        MAPPINGS.delete(url2);
+    });
+
+    worker.on("exit", (code) => {
+        console.log(`Bridge ${url2} <-> ${settings.socket}://${settings.host}:${settings.port} finished`, code);
+        MAPPINGS.delete(url2);
+    });
+
+    MAPPINGS.set(url2, worker);
+    return Promise.resolve();
+
 }
+
 
 module.exports = (map, ws) => {
 
+    MAPPINGS.forEach(async (worker) => {
+        console.log("Termoinate worker", worker.threadId);
+        await worker.terminate();
+    });
+
     console.log("ws protocol: %s, map:", ws.protocol, map);
 
-    ws.on("message", (data) => {
+    ws.on("message", (msg) => {
 
         // parse data
-        data = JSON.parse(data);
+        msg = JSON.parse(msg);
 
         // handle only device specifiy events.
         // Like added & updated devices rsp. interfaces
-        if (data.component === "devices" && ["added", "updated"].includes(data.event)) {
+        if (msg.component === "devices" && ["add", "update"].includes(msg.event)) {
 
-            console.log("Handle updated/added devices", data);
+            console.log("Handle updated/added devices", msg);
+
+            msg.data.interfaces.forEach((iface) => {
+
+                // parse websocket urls
+                let url1 = new url.URL(ws.url);
+                let url2 = new url.URL(`${process.env.BACKEND_URL}/api/devices/${msg.data._id}/interfaces/${iface._id}`);
+
+                url2.protocol = url1.protocol;
+
+                spawn(url2, iface.settings);
+
+            });
 
         } else {
 
@@ -95,9 +86,9 @@ module.exports = (map, ws) => {
         // override http(s) with ws(s)
         url2.protocol = url1.protocol;
 
-        console.log(`Bridge "%s" <-> ${settings.socket}://${settings.host}:${settings.port}`, url2);
+        console.log(`Bridge ${url2} <-> ${settings.socket}://${settings.host}:${settings.port}`);
 
-        bridge(url2, settings);
+        spawn(url2, settings);
 
     }
 
