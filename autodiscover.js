58641f4217a662502de6329f788284a3e0936608
mStirner <marc.stirner@hotmail.de>
2022-12-23 20:23:12 +0100

    minor bug fix

--- autodiscover.js
+++ autodiscover.js
@@ -7,177 +7,179 @@ const { xml2json } = require("xml-js");
 const request = require("./request");
 
 
+if (process.env.ENABLE_SSDP === "true") {
+
+    let uri = new url.URL(process.env.BACKEND_URL);
+    uri.protocol = (process.env.BACKEND_PROTOCOL === "https" ? "wss" : "ws");
+    uri.pathname = "/api/ssdp";
+    const ws = new WebSocket(uri);
 
 
-let uri = new url.URL(process.env.BACKEND_URL);
-uri.protocol = (process.env.BACKEND_PROTOCOL === "https" ? "wss" : "ws");
-uri.pathname = "/api/ssdp";
-const ws = new WebSocket(uri);
-
-
-const socket = dgram.createSocket({
-    type: "udp4",
-    reuseAddr: true
-});
-
-
-async function parseHeader(msg, { address, port }) {
-
-    let headers = {};
-    let lines = msg.toString().split("\r\n");
-    let type = lines[0];
-
-    if (type.endsWith("* HTTP/1.1")) {
-        type = type.split(" ")[0];
-    } else if (type === "HTTP/1.1 200 OK") {
-        type = "SEARCH-RESPONSE";
-    } else {
-        console.log("Unknwon type");
-    }
-
-    // store head key/value pair in object
-    lines.slice(1, lines.length - 2).forEach((line) => {
-
-        let [key, ...value] = line.split(":");
-
-        let k = key.toLowerCase();
-        let v = value.join(":").trim().replaceAll(`"`, "");
-
-        headers[k] = v;
-
+    const socket = dgram.createSocket({
+        type: "udp4",
+        reuseAddr: true
     });
 
-    // add additional client headers
-    headers["X-REMOTE-ADDRESS"] = address;
-    headers["X-REMOTE-PORT"] = port;
 
-    // for better handling, convert to lowercase
-    type = type.toLowerCase();
+    async function parseHeader(msg, { address, port }) {
+
+        let headers = {};
+        let lines = msg.toString().split("\r\n");
+        let type = lines[0];
+
+        if (type.endsWith("* HTTP/1.1")) {
+            type = type.split(" ")[0];
+        } else if (type === "HTTP/1.1 200 OK") {
+            type = "SEARCH-RESPONSE";
+        } else {
+            console.log("Unknwon type");
+        }
+
+        // store head key/value pair in object
+        lines.slice(1, lines.length - 2).forEach((line) => {
+
+            let [key, ...value] = line.split(":");
+
+            let k = key.toLowerCase();
+            let v = value.join(":").trim().replaceAll(`"`, "");
+
+            headers[k] = v;
+
+        });
+
+        // add additional client headers
+        headers["X-REMOTE-ADDRESS"] = address;
+        headers["X-REMOTE-PORT"] = port;
+
+        // for better handling, convert to lowercase
+        type = type.toLowerCase();
 
 
-    if (!["m-search", "notify", "search-response"].includes(type)) {
-        console.log("UNKNOWN SSDP MESSAGE");
-        throw new Error(`unknown ssdp message type ${type}`);
+        if (!["m-search", "notify", "search-response"].includes(type)) {
+            console.log("UNKNOWN SSDP MESSAGE");
+            throw new Error(`unknown ssdp message type ${type}`);
+        }
+
+        return {
+            headers,
+            type
+        };
+
     }
 
-    return {
-        headers,
-        type
-    };
+    function fetchLocation(uri) {
+        return new Promise((resolve, reject) => {
+            request(uri, (err, result) => {
+                if (err) {
 
-}
+                    reject(err);
 
-function fetchLocation(uri) {
-    return new Promise((resolve, reject) => {
-        request(uri, (err, result) => {
-            if (err) {
+                } else {
 
-                reject(err);
+                    let { body } = result;
 
-            } else {
+                    let json = xml2json(body.toString(), {
+                        compact: true,
+                        spaces: 4
+                    });
 
-                let { body } = result;
+                    resolve(json);
 
-                let json = xml2json(body.toString(), {
-                    compact: true,
-                    spaces: 4
+                }
+            });
+        });
+    }
+
+    socket.on("message", async (msg, { address, port }) => {
+        if (ws.readyState === ws.OPEN) {
+            try {
+
+                // prase header & set additional remote header
+                // X-REMOTE-ADDRESS = address
+                // X-REMOTE-PORT = port
+                let { headers, type } = await parseHeader(msg, {
+                    address,
+                    port
                 });
 
-                resolve(json);
 
-            }
-        });
-    });
-}
-
-socket.on("message", async (msg, { address, port }) => {
-    if (ws.readyState === ws.OPEN) {
-        try {
-
-            // prase header & set additional remote header
-            // X-REMOTE-ADDRESS = address
-            // X-REMOTE-PORT = port
-            let { headers, type } = await parseHeader(msg, {
-                address,
-                port
-            });
+                // extract location & nts
+                // (to fetch description)
+                let { location, nts } = headers;
 
 
-            // extract location & nts
-            // (to fetch description)
-            let { location, nts } = headers;
-
-
-            // create from header object
-            // a new `msg` like string
-            /*
-            let head = Object.keys(headers).map((key) => {
-                return `${key}=${headers[key]}\r\n`;
-            });
-            */
-
-
-            // check if we handle a notification & device is alive
-            // if so do a http requestion to the location header value
-            if (type === "notify" && nts === "ssdp:alive" && location) {
-
-                let description = await fetchLocation(location);
-
-                let data = Buffer.concat([
-                    //Buffer.from(type),    // DRAFT!
-                    //Buffer.from(head),    // DRAFT!
-                    Buffer.from(msg),
-                    Buffer.from(description),
-                    Buffer.from("\r\n"),
-                    Buffer.from("\r\n")
-                ]);
-
-                // feedback
-                //console.log(`Send to server from client udp://${address}:${port} `, data.toString());
-
-                ws.send(data);
-
-            } else {
-
+                // create from header object
+                // a new `msg` like string
                 /*
-                let data = Buffer.concat([
-                    Buffer.from(type),
-                    Buffer.from(head)
-                ]);
+                let head = Object.keys(headers).map((key) => {
+                    return `${key}=${headers[key]}\r\n`;
+                });
                 */
 
-                ws.send(msg);
+
+                // check if we handle a notification & device is alive
+                // if so do a http requestion to the location header value
+                if (type === "notify" && nts === "ssdp:alive" && location) {
+
+                    let description = await fetchLocation(location);
+
+                    let data = Buffer.concat([
+                        //Buffer.from(type),    // DRAFT!
+                        //Buffer.from(head),    // DRAFT!
+                        Buffer.from(msg),
+                        Buffer.from(description),
+                        Buffer.from("\r\n"),
+                        Buffer.from("\r\n")
+                    ]);
+
+                    // feedback
+                    //console.log(`Send to server from client udp://${address}:${port} `, data.toString());
+
+                    ws.send(data);
+
+                } else {
+
+                    /*
+                    let data = Buffer.concat([
+                        Buffer.from(type),
+                        Buffer.from(head)
+                    ]);
+                    */
+
+                    ws.send(msg);
+
+                }
+
+
+            } catch (err) {
+
+                console.error(err);
 
             }
-
-
-        } catch (err) {
-
-            console.error(err);
-
         }
-    }
-});
+    });
 
 
-socket.on("listening", () => {
+    socket.on("listening", () => {
 
-    const address = socket.address();
-    console.log(`server listening ${address.address}:${address.port}`);
+        const address = socket.address();
+        console.log(`server listening ${address.address}:${address.port}`);
 
-    console.log("udp server listening");
+        console.log("udp server listening");
 
-    socket.addMembership("239.255.255.250");
+        socket.addMembership("239.255.255.250");
 
-});
+    });
 
 
 
-ws.on("open", () => {
+    ws.on("open", () => {
 
-    console.log(`Connected to  ${ws.url}`);
+        console.log(`Connected to  ${ws.url}`);
 
-    // bind socket
-    socket.bind(1900, "0.0.0.0");
+        // bind socket
+        socket.bind(1900, "0.0.0.0");
 
-});
+    });
+
+}
